12e3b39645ac893b6ab5cfc2103ac2e5f9f3cd69
zhl003 <845789386@qq.com>
2022-03-25 15:52:29 +0800

    rebuild debian images

---
+++ bin/common/build_extension.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p $1
+ghRepoCloneLatestRelease ()
+{
+    [[ ${1} =~ / ]] &&
+        wget -qO- https://github.com/${1}/$(curl -s https://github.com/${1}/releases |
+            grep -m1 -Eo "archive/refs/tags/[^/]+\.tar\.gz") |
+                tar --strip-components=1 -xzv >/dev/null
+}
--- bin/postgres_common/postgres/start.sh
+++ bin/postgres_common/postgres/start.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+set -Ex
 RADONDB_DIR=${RADONDB_DIR:-'/opt/radondb'}
 source "${RADONDB_DIR}/bin/common_lib.sh"
 enable_debugging
